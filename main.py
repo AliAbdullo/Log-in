711d90c7a844d8205b354206d74cc36f12755193
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2022-05-23 05:09:23 +0000

    Qayta yozib tozalangan holda joylandi

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 class User:
-  def __init__(self, ism, familiyasi, oismi, traqami, email):
-    self.ism=ism
-    self.lastname=familiyasi
-    self.otasiningIsmi=oismi
-    self.telefonNumber=traqami
-    self.emanzil=email
-    
+  """User nomli class """
+  def __init__(self, nickname, ism, familya, email):
+    self.nick=nickname
+    self.name= ism 
+    self.surname = familya
+    self.email = email
+
+  def get_nick(self):
+    """Foydalanuvchi Nicknameni qaytaradi"""
+    return self.nick
+
   def get_name(self):
-    """Ismni qaytarib beradi"""
-    return self.ism
+    """Foydalanuvchi ismini qaytaradi"""
+    return self.name
+
+  def get_surname(self):
+    """Foydalanuvchi familyasini qaytaradi"""
+    return self.surname
+
+  def get_email(self):
+    """Foydalanuvchi emailni qaytaradi"""
+    return self.email
 
   def get_info(self):
-    """Foydalanuvchi malumotlari """
-    print( f"Foydalanuvchi:{self.ism}, ismi:{self.lastname} {self.otasiningIsmi}, telefon raqami: {self.telefonNumber}")
+    """Foydalanuvchi xaqida ma'lumot qaytaradi """
+    return f"Foydalanuvchi:{self.nick}, ismi:{self.name} {self.surname}, email:{self.email}"
 
 
-
-user1 = User("Vali","Saliy","Ani", +998902067510, "zizizizi")  
-people= User("Ali",  "Abdulloh", "Abdumutalogli", +998901695979, "abdjf")
-print(people.get_name())
-print(user1.get_info())
-
+user1= User("Ali","Abdulloh","Abdug'aniyev","aliabdulloh@mail.ru") 
+user2= User("Vali","Vali","Abdug'aniyev","valivali@mail.ru") 
+user3= User("Sali","Sali","Abdug'aniyev","salisali@mail.ru")  
+user4= User("Lali","Lali","Abdug'aniyev","Lalilali@mail.ru")  
+user1= User("Ali","Abdulloh","Abdug'aniyev","aliabdulloh@mail.ru")  
+user5= User("Ali","Abdulloh","Abdug'aniyev","aliabdulloh@mail.ru")  
+user6= User("Ali","Abdulloh","Abdug'aniyev","aliabdulloh@mail.ru") 
   
+
+print(user2.get_info())
